dc8e30e795de4bd304a729f9f508e9684e1fde54
ZHAHIRAZHAR <muhammadzhahir@graduate.utm.my>
2024-12-17 09:12:24 +0800

    add background to staff dashboard

--- RCS/resources/views/complaints/index.blade.php
+++ RCS/resources/views/complaints/index.blade.php
@@ -6,8 +6,10 @@
     <title>Complaints List</title>
     <!-- Add Bootstrap CSS for styling -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
 </head>
-<body>
+<body style="background: url('/images/51696.jpg') no-repeat center center fixed; background-size: cover;">
     <div class="container mt-5">
         <h1 class="mb-4">Complaints List</h1>
 
